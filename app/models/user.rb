0693fc33b5398d7af98c27e8f8821069ac8ab9a2
kana7825 <petitbirdsong@gmail.com>
2019-10-21 18:29:45 +0000

    4章4.4.6コメント後終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  # ↑この継承の働きによりActive Recordのﾒｿｯﾄﾞが使える
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50}
+  # ↑検証　名前の存在性あること、長さは50文字以下
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # ↑値が変化しないため、定数として定義＝引数として正規表現（Reguler Expression）、この正規表現でﾒﾙｱﾄﾞのﾌｫｰﾏｯﾄを検証できる
   validates :email, presence: true, length: { maximum:100},
                     format:{with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+                    uniqueness: true# 一意性の検証
 end
 
