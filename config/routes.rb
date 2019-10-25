cf405a0c867af76c7a7bf343d8be3aa1f237789f
kana7825 <petitbirdsong@gmail.com>
2019-10-25 16:11:05 +0000

    5章5.5.4終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#top'# static_pages_controller.rbのtpoアクションはこれを呼びにきてView（Top.html.erb)を出力しtopページを出している
+  root 'static_pages#top'
+  # static_pages_controller.rbのtpoアクションはこれを呼びにきてView（Top.html.erb)を出力しtopページを出している
   get '/signup', to: 'users#new'
   resources :users
 end
