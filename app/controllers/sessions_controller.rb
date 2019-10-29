32b7550ea25cdddd86049987cde46f89c42875f7
kana7825 <petitbirdsong@gmail.com>
2019-10-29 14:32:33 +0000

    6章6.3.1ログイン用のﾒｿｯﾄﾞを作ろう

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  
   def new
   end
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) 
-      # ログイン後にユーザー情報ページにリダイレクトします。
+     log_in user
+     reirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
