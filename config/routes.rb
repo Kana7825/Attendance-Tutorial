79a831be8df035e90cf7e77a095c3a2351e89fc0
kana7825 <petitbirdsong@gmail.com>
2019-10-29 12:54:55 +0000

    6章6.2.1終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  # static_pages_controller.rbのtpoアクションはこれを呼びにきてView（Top.html.erb)を出力しtopページを出している
+  # static_pages_controller.rbのtopアクションはこれを呼びにきてView（Top.html.erb)を出力しtopページを出している
   get '/signup', to: 'users#new'
+  
+  # ログイン機能
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
 end
