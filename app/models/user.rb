65b709eed95864fac3ea5e0c9ba6a6bef627c8eb
kana7825 <petitbirdsong@gmail.com>
2019-10-31 14:43:53 +0000

    7章7.1.3終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   # ↑この継承の働きによりActive Recordのﾒｿｯﾄﾞが使える
+  attr_accessor :remember_token
+  # ↑「remember_token」という仮想の属性を作成します。
   before_save { self.email = email.downcase }
   #　↑before_saveメソッドにブロック{self.email=...｝を渡してﾕｰｻﾞｰのﾒﾙｱﾄﾞを設定
   #　↑現在のﾒﾙｱﾄﾞ（self.email）の値をdowncaseﾒｿｯﾄﾞを使って小文字に変換
@@ -12,5 +14,27 @@ class User < ApplicationRecord
                     uniqueness: true# 一意性の検証
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+
+# 渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+         BCrypt::Engine::MIN_COST
+      else
+         BCrypt::Engine.cost
+    end
+  BCrypt::Password.create(string, cost: cost)
 end
 
+# ランダムなトークンを返します。
+  def User.new_token
+   SecureRandom.urlsafe_base64
+  end
+
+# 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+end
