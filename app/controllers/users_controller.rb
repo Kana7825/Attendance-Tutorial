b43e336b030c521259333f404e1a564d231e0c0d
kana7825 <petitbirdsong@gmail.com>
2019-11-07 12:37:29 +0000

    8章8.2.2終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :loggid_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -20,19 +21,17 @@ class UsersController < ApplicationController
     end
   end
  
- def edit
-   @user = User.find(params[:id])
- end
+  def edit
+  end
  
- def update
-   @user = User.find(params[:id])
-   if @user.update_attributes(user_params)
+  def update
+    if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
-   else 
+    else 
      render :edit
-   end
-end  
+    end
+  end  
 
 
   private
@@ -43,13 +42,25 @@ end
   end
   
   # beforeフィルター
+ 
+  # paramsハッシュからユーザーを取得します。
+   def set_user # set_userメソッドは上のbefore_actionで作ってるものかな？
+    @user = User.find(params[:id])
+   end
   
-  # ログイン済みのユーザーが確認します。
+  # ログイン済みのユーザーか確認します。
   def logged_in_user
     unless logged_in?
     flash[:danger] = "ログインしてください。"
     redirect_to login_url
     end
   end
+
+#　アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
 end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,11 +34,18 @@ module SessionsHelper
     elsif (user_id = cookies.signed[:user_id])
       user= User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
-        log_in user@current_user = user
+        log_in user
+        @current_user = user
       end
     end
   end  
-  
+ 
+ # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end
+   
+ # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。 
   def logged_in?
     !current_user.nil?
   end
