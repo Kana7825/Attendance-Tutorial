f56f00cb2a4a771059252a24cc4649f7d4ae9bf0
kana7825 <petitbirdsong@gmail.com>
2019-11-09 13:55:59 +0000

    8章おまけ　ロケールファイルの読み込み設定　終了

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require 'rails/all'
 
 require 'rails/all'
 
@@ -11,7 +12,8 @@ module AttendanceApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Asia/Tokyo'
-
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
