dccd173768e3db7efdd9db861dd226a69e5984e1
kana7825 <petitbirdsong@gmail.com>
2019-11-16 15:36:28 +0000

    11章11.3.2例外処理を含めた更新を追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,15 +1,14 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
-  
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
-  
+
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
-  # ↑定数　定数は値を変えることができない。大文字表記。
+
   def update
-    @user =User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
-    # ↓ 出勤時間が未登録であることを判断します。
+    # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
@@ -25,16 +24,29 @@ class AttendancesController < ApplicationController
     end
     redirect_to @user
   end
-  
+
   def edit_one_month
   end
   
   def update_one_month
+  ActiveRecord::Base.transaction do # トランザクションを開始します。
+    attendances_params.each do |id, item|
+      # ↑このattendances_paramsを定義してるのは、
+      # この下のprivateの中のdef attendances_paramsです！
+      attendance = Attendance.find(id)
+      attendance.update_attributes!(item)
+    end
   end
+  flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+  redirect_to user_url(date: params[:date])
+rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+  flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+  redirect_to attendances_edit_one_month_user_url(date: params[:date])
+end
   
   private
    # 1ヶ月分の勤怠情報を扱います。
-  def attendance_params
+  def attendances_params
     params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
   end
 end
